3166c2c19c1406274e088340634141007f769de3
Haroon Khokhar <haroonriaz6565@gmail.com>
2025-09-03 22:46:11 +0500

    done (cors)

--- honda-backend/src/config/index.js
+++ honda-backend/src/config/index.js
@@ -4,6 +4,7 @@ export const PORT = Number(process.env.PORT || 4000);
 export const MONGO_URI = process.env.MONGO_URI;
 export const CORS_ORIGIN = [
   'https://honda-software-main.vercel.app',
+  'https://www.masterhonda.com',
   'http://localhost:3000',
   'http://localhost:5000',
   'http://127.0.0.1:3000',
